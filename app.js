a2d7b92fc11a94fcc9a13f855aa0403f832fe46a
Arch <jacob.archer@snhu.edu>
2026-06-14 12:18:04 -1000

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('./app_server/models/db');
+require('./app_api/models/db');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,10 +7,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-const { travel } = require('./app_server/controllers/travel');
-var handlebars = require('hbs');
-var travelRouter = require('./app_server/routes/travel');
 
+
+var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
+var handlebars = require('hbs');
 var app = express();
 
 // view engine setup
@@ -29,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,25 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json'
+  }
+};
 
 /* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: "Travlr Getaways", trips});
+const travel = async (req, res) => {
+  await fetch(tripsEndpoint, options)
+    .then(res => res.json())
+    .then(json => {
+      res.render('travel', {
+        title: 'Travlr Getaways',
+        trips: json
+      });
+    })
+    .catch(err => res.status(500).send(err.message));
 };
 
 module.exports = {
-    travel
+  travel
 };
