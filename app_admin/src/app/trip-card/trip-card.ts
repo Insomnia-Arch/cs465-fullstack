7fd586bdf4bf4d7ad74dbfbd5cff6d0433bb5400
Arch <jacob.archer@snhu.edu>
2026-06-28 13:17:11 -1000

    Module 7 completed

--- app_admin/src/app/trip-card/trip-card.ts
+++ app_admin/src/app/trip-card/trip-card.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Trip } from '../models/trip';
+import { Authentication } from '../services/authentication';
 
 @Component({
   selector: 'app-trip-card',
@@ -15,7 +16,7 @@ import { Trip } from '../models/trip';
 export class TripCard implements OnInit {
   @Input('trip') trip!: Trip;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authenticationService: Authentication) { }
   ngOnInit(): void {
   }
 public editTrip(trip:Trip) {
@@ -23,5 +24,6 @@ public editTrip(trip:Trip) {
   localStorage.setItem('tripCode', trip.code);
   this.router.navigate(['edit-trip']);
 }
+
 
 } 
